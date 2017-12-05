789a0a30adb1b06807e4e51f697acb0447366142
Michael Segulja <michael@segulja.com>
2017-12-05 09:00:47 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# segulja_base
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: segulja_base
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
