2738e11a9a42482641fa0f58f3a6e3bddc77cecd
Michael Segulja <michael@segulja.com>
2018-02-13 22:15:05 -0600

    Comment out the reboot; Add test for not_if reboot gate file; Bumped version to 0.3.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures segulja_base'
 long_description 'Installs/Configures segulja_base'
-version '0.3.0'
+version '0.3.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends 'chef-client'
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,19 +8,19 @@ include_recipe 'chef-client::default'
 include_recipe 'segulja_users::default'
 include_recipe 'segulja_packages::default'
 include_recipe 'segulja_sudo::default'
-include_recipe 'segulja_autofs::default'
+# include_recipe 'segulja_autofs::default'
 include_recipe 'segulja_hostfile::default'
 
-execute 'Reboot after initial build' do
-  command 'shutdown -r 1'
-  action :run
-  not_if { File.exist?('/root/initial-build-complete') }
-end
+# execute 'Reboot after initial build' do
+#   command 'shutdown -r 1'
+#   action :run
+#   not_if { File.exist?('/root/.initial-build-complete') }
+# end
 
-file '/root/initial-build-complete' do
+file '/root/.initial-build-complete' do
   content 'DO NOT DELETE'
   owner 'root'
   group 'root'
-  mode '0755'
+  mode '0400'
   action :create
 end
