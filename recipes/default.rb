3eca45a48d1497a50a01545589d24124cfe3f36b
Michael Segulja <michael@segulja.com>
2018-05-18 10:48:07 -0500

    Changes:
      - Added the segulja_packages cookbook
      - Bumped version to 0.3.3
    
    On branch new-server
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   .kitchen.yml
    	modified:   metadata.rb
    	modified:   recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,7 +6,7 @@
 
 include_recipe 'chef-client::default'
 include_recipe 'segulja_users::default'
-# include_recipe 'segulja_packages::default'
+include_recipe 'segulja_packages::default'
 include_recipe 'segulja_sudo::default'
 # include_recipe 'segulja_autofs::default'
 include_recipe 'segulja_hostfile::default'
