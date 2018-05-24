5f6d5c630fee436ff6a6d323ceb80f2f5f340098
Michael Segulja <michael@segulja.com>
2018-05-23 22:58:39 -0500

    Bump version to 0.3.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures segulja_base'
 long_description 'Installs/Configures segulja_base'
-version '0.3.4'
+version '0.3.5'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends 'chef-client'
